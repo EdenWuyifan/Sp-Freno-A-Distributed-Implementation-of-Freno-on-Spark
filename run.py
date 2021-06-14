456b186af2283e3eab4efef5ee7cf96c5fbd7dcc
Unknown <yfw215@nyu.edu>
2021-06-14 17:02:58 +0800

    update run

--- run.py
+++ run.py
@@ -7,24 +7,28 @@ from utils import distFreno
 import time
 import os
 import numpy as np
-import argparse
+#import argparse
 
 memory = '10g'
 pyspark_submit_args = ' --driver-memory ' + memory + ' pyspark-shell'
 os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
 os.environ["PYTHONHASHSEED"]=str(232)
-
+'''
 parser = argparse.ArgumentParser(description='argparse')
 parser.add_argument('--database', '-d', help='database name', required=True)
 parser.add_argument('--minsup', '-m', help='min support percentage', required=True)
 parser.add_argument('--partition', '-p', help='num of workers', required=True)
 args = parser.parse_args()
-
+'''
 
 def main():
-    database = args.database
-    support = int(args.minsup)/100
-    partition = int(args.partition)
+    #database = args.database
+    #support = int(args.minsup)/100
+    #partition = int(args.partition)
+    
+    database = ["retail"]
+    support = [40]
+    partition = 8
 
     conf = SparkConf().setAppName("")
     sc = SparkContext.getOrCreate(conf=conf)
@@ -39,10 +43,11 @@ def main():
         schema.add("test{}".format(i+1), FloatType(), True)
     #experiments = []
 
-    #for f in testFiles:
-    #for s in support:
-    res = distFreno("./databases/{}.txt".format(database), support, sc, partition)
-    print(res)
+    for f in database:
+        for s in support:
+            res = distFreno("./databases/{}.txt".format(database), support, sc, partition)
+            print(res)
+    
     sc.stop()
     return res
     
